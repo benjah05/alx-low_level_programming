6d452e14c70e6a87b2da8a7d032a60d37fb2435c
benjah05 <senzelebenjamine@gmail.com>
2024-06-20 11:37:32 -0700

    Advanced: Intel & UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - program entry point
+ * Return: 1
+ */
+int main(void)
+{printf("%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
