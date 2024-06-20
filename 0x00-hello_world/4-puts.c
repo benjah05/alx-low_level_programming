e8d2107cb373ee603be9b6168f3775f4894d4854
benjah05 <senzelebenjamine@gmail.com>
2024-06-20 10:36:24 -0700

    Preprocessor, Compiler, Assembler, Name, Hello puts, Hello printf, Size is not grandeur and territory does not make a nation

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - program entry point
+ * Return: void
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - program entry point
+ * Return: void
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
