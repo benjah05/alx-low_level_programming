1bf229a4184eefcaa2a70bffd41232703eace14d
benjah05 <senzelebenjamine@gmail.com>
2024-06-21 07:49:54 -0700

    Fix commit 1.3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ int main(void)
 	else if (remainder == 0)
 		printf("Last digit of %d is %d and is 0\n", n, remainder);
 	else if (remainder < 6 && remainder != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, remainder);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
 	{
-		if (alphabt != 'e')
+		if (alphabt != 'e'|| alphabt != 'q')
 			putchar(alphabt);
 	}
 	putchar('\n');
