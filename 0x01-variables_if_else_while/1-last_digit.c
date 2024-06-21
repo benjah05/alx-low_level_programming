2fb8e0acf9a5a40977f72f3740c299ba89b6046f
benjah05 <senzelebenjamine@gmail.com>
2024-06-21 07:41:14 -0700

    Fix commit 1.2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,10 +18,10 @@ int main(void)
 	/* your code goes there */
 	remainder = n % 10;
 	if (remainder > 5)
-		printf("Last digit of %d is %d and is greater than 5 and not 0\n", n, remainder);
+		printf("Last digit of %d is %d and is greater than 5\n", n, remainder);
 	else if (remainder == 0)
 		printf("Last digit of %d is %d and is 0\n", n, remainder);
 	else if (remainder < 6 && remainder != 0)
-		printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, remainder);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, remainder);
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,6 +13,6 @@ int main(void)
 		if (alphabt != 'e')
 			putchar(alphabt);
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
