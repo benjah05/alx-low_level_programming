1d49a7adfa0e925b13575ce6df1ec2d17cc35c96
benjah05 <senzelebenjamine@gmail.com>
2024-06-21 19:09:15 -0700

    Fix commit 3.16

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,9 +14,9 @@ int main(void)
 	int i, j;
 
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			number_1 = i / 10;
 			number_2 = i % 10;
