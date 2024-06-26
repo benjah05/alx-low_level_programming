30d5be638e2bcb04c8e836fb5190a7c63021ea93
benjah05 <senzelebenjamine@gmail.com>
2024-06-26 10:03:57 -0700

    Multiple mains; Like, comment, subscribe & 0 > 97?

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - check if numbers are either positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
