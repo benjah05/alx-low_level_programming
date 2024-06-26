22b5f33f310d882774e2eada09aff3e64b54e5ef
benjah05 <senzelebenjamine@gmail.com>
2024-06-25 18:26:44 -0700

    Fix commit 3.3

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar('0' + product);
 				}
-				else if (product >= 10)
+				else if (product >= 10 && product < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
