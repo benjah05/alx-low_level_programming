59308185a66d9afb55c059bb75085a0a0ef37f20
benjah05 <senzelebenjamine@gmail.com>
2024-06-27 08:07:52 -0700

    isupper, isdigit, Collaboration is multiplication, The numbers speak for themselves, I believe in nuumbers and signs & Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include<stdio.h>
+void _putchar(char  c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
+#endif
