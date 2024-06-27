f7be21d5393a1e3f92328f9a0c273bca55171341
benjah05 <senzelebenjamine@gmail.com>
2024-06-27 10:21:55 -0700

    Fizzbuzz to FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("Fizzbuzz ");
+			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
