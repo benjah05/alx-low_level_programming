aeaa7c2cc8f0fffe57f91b8216e867463544c658
benjah05 <senzelebenjamine@gmail.com>
2024-06-27 18:54:25 -0700

    Adjust code logic

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,23 @@
-#include<stdio.h>
 #include "main.h"
 /**
- * print_number - print an integer
- * @n: integer
- * Return: void
- */
+* print_number - Prints a number
+* @n: integer
+* Return: void
+*/
 void print_number(int n)
 {
 	unsigned int i;
 
-	i = n;
-	if (i < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		i = -i;
+		i = -n;
 	}
-	if (i < 10)
-		_putchar('0' + i);
 	else
+		i = n;
+	if (i >= 10)
 	{
 		print_number(i / 10);
-		_putchar('0' + (i % 10));
 	}
-
+	_putchar('0' + (i % 10));
 }
