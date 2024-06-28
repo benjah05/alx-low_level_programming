02aae3484f5a7a641f7a779e6ce2c2254302e003
benjah05 <senzelebenjamine@gmail.com>
2024-06-27 19:31:38 -0700

    Fix commit 100th:)

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,14 +5,14 @@
 */
 int main(void)
 {
-	long int num = 612852475143;
-	int i = 1;
+	long num = 612852475143;
+	long i;
 
 	while (i < num / 2)
 	{
 		if (num % i == 0)
 		{
-			num /= i;
+			num /= 2;
 			continue;
 		}
 		for (i = 3; i < num / 2; i += 2)
