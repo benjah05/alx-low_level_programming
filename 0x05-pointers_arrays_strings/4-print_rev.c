6f7b8a29bda96bb3a2837e56abe374aea8e97e46
benjah05 <senzelebenjamine@gmail.com>
2024-07-01 14:52:41 -0700

    Fix print_rev() to take any string length

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,10 @@
  */
 void print_rev(char *s)
 {
-	char str[100];
-	int i, j, size;
+	int i, size = strlen(s);
+	
+	for (i = size - 1; i >= 0; i--)
+		printf("%c", s[i]);
+	printf("\n");
 
-	size = strlen(s);
-	for (i = 0, j = size - 1; i < size && j >= 0; i++, j--)
-		str[i] = s[j];
-	str[size] = '\0';
-	printf("%s\n", str);
 }
