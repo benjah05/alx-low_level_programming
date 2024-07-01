2d9e706e4a622d59cd303a4877fba6ac3f502143
benjah05 <senzelebenjamine@gmail.com>
2024-07-01 15:12:49 -0700

    Fix code logic to take any string length

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,14 @@
  */
 void rev_string(char *s)
 {
-	char str[10];
+	char temp;
 	int i, j, size;
 
 	size = strlen(s);
-	for (i = 0, j = size - 1; i < size && j >= 0; i++, j--)
-		str[i] = s[j];
-	str[size] = '\0';
-	for (i = 0, j = 0; i < size && j < size; i++, j++)
-		s[i] = str[j];
-	s[size] = '\0';
+	for (i = 0, j = size - 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
