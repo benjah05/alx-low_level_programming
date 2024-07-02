fb4bf907dd85b54ae549b89a3f87681f716c6f04
benjah05 <senzelebenjamine@gmail.com>
2024-07-01 19:02:39 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,8 +5,19 @@
  * main - generate valid passwords
  * Return: 0
  */
-int main()
+int main(void)
 {
+	int sum = 0, n;
+	int rand_char;
 
 	srand(time(NULL));
+	while (sum < 2772 - 127)
+	{
+		rand_char = rand() % 94 + 33;
+		sum += rand_char;
+		putchar(rand_char);
+	}
+	n = 2772 - sum;
+	putchar(n);
+	return (0);
 }
