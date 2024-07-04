7aa448866938aa686d059f1fb40c2a3cd7a5d0f7
benjah05 <senzelebenjamine@gmail.com>
2024-07-03 18:31:03 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+/**
+ * cap_string - capitalize all words of a string
+ * @str: string to capitalize
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i, j, str_size;
+	char separators[14] = { 32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125 };
+
+	str_size = strlen(str);
+	for (i = 0; i < str_size; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (separators[j] == (int) str[i])
+			{
+				if (islower(str[i + 1]))
+				{
+					str[i + 1] = (char) ((int) str[i + 1] - 32);
+				}
+				break;
+			}
+		}
+	}
+	return (str);
+}
