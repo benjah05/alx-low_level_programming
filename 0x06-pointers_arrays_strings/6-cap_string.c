0361cfd7bb7bbb23177e3942cfd8f955f31ff562
benjah05 <senzelebenjamine@gmail.com>
2024-07-03 20:11:22 -0700

    Prepare for the worst

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,21 +8,20 @@
  */
 char *cap_string(char *str)
 {
-	int i, str_size;
-	long int unsigned j;
+	int i, j, str_size;
 	char separators[] = { 32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125 };
-	/* ASCII values: ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'*/
+	/* ASCII: ' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'*/
 
 	str_size = strlen(str);
 	for (i = 0; i < str_size; i++)
 	{
-		for (j = 0; j < sizeof(separators) / sizeof(separators[0]); j++)
+		for (j = 0; j < 13; j++)
 		{
 			if (separators[j] == (int) str[i])
 			{
 				if (i + 1 < str_size && islower(str[i + 1]))
 				{
-					str[i + 1] = (char) ((int) str[i + 1] - 32);
+					str[i + 1] = (char) toupper(str[i + 1]);
 				}
 			}
 		}
