065cde1b4b42d60134fbe96afbadc4fe5adda52e
benjah05 <senzelebenjamine@gmail.com>
2024-07-08 13:03:24 -0700

    Fix code to solve for all cases

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i, size = strlen(s);
-
-	for (i = 0; i < size; i++)
+	unsigned int i = 0;
+	
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return (&s[i]);
+		i++;
 	}
 	return (NULL);
 
