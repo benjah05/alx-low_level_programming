919cec8745cba1c0b1e52e474cb1a4fa72a7e9c3
benjah05 <senzelebenjamine@gmail.com>
2024-07-10 16:31:31 -0700

    remove staticvariables

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
- * _sqrt_recursion - find natural squrae root of a number
+ * _sqrt_root - find the square root of n
+ * @i: iteration from 1 to square root of n
+ * Return: square root of n
+ */
+int _sqrt_root(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt_root(n, i + 1));
+}
+/**                                           * _sqrt_recursion - find natural square root of a number
  * @n: the number of int type
  * Return: the natural square root of n
  */
 int _sqrt_recursion(int n)
 {
-	static int i = 1;
-
-	if (n < 0)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	if (i * i > n)
-		return (-1);
-	i++;
-	return (_sqrt_recursion(n));
+        if (n < 0)
+                return (-1);
+        return (_sqrt_root(n, 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,27 @@
 #include "main.h"
 /**
- * is_prime_number - check if a number is prime or not
+ * _check_prime - check if a number is prime or not
+ * @n: the number
+ * @i: iteration from 1 to any n divisors
+ * Return: 1 if prime, 0 if not
+ */
+int _check_prime(int n, int i)
+{
+	if (n % i == 0 && n != i)
+		return (0);
+	if (i * i > n)
+		return (1);
+	return (_check_prime(n, i + 1));
+}
+/**                                           * is_prime_number - check if a number is prime or not
  * @n: the number
  * Return: 1 if prime, 0 if not
  */
 int is_prime_number(int n)
 {
-	static int i = 2;
-
 	if (n <= 1)
 		return (0);
 	if (n == 2)
 		return (1);
-	if (n % i == 0 && n != i)
-		return (0);
-	if (i * i > n)
-		return (1);
-	i++;
-	return (is_prime_number(n));
+	return (_check_prime(n, 2));
 }
