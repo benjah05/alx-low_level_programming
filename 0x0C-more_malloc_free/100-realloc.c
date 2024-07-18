f278e8ac857c5cd0579980039cc7c3176df370f8
benjah05 <senzelebenjamine@gmail.com>
2024-07-18 11:23:37 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (newPtr == NULL)
 		return (NULL);
 	if (new_size > old_size)
-		return (ptr);
+		return (newPtr);
 	for (i = 0; i < new_size; i++)
 		*((char *)newPtr + i) = 0;
 	return (newPtr);
