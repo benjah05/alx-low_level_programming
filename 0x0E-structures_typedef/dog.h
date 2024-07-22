0b1f83716f9e28d00b2ab2fac2ad0c5c6f3a4eeb
benjah05 <senzelebenjamine@gmail.com>
2024-07-22 11:31:15 -0700

    Add description for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - strcut dog
+ * @name: the name of the dawg
+ * @age: dog's age
+ * @owner: the dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
