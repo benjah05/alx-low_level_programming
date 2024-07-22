4ce738dd60724e16699c56ea97693cdbbb3d93a7
benjah05 <senzelebenjamine@gmail.com>
2024-07-22 07:03:14 -0700

    Object-like Macro, Pi, File name, Function-like macro & SUM

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _OBJECTLIKEMACRO_H
+#define _OBJECTLIKEMACRO_H
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTIONMACRO_H
+#define _FUNCTIONMACRO_H
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
