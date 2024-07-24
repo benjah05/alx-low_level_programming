960419e20f2a991e00fac073517ed3672a096218
benjah05 <senzelebenjamine@gmail.com>
2024-07-24 09:18:12 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: the name
+ * @f: function pointer to whether to print name in uppercase or as is
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTIONPOINTERS_H
+#define _FUNCTIONPOINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
