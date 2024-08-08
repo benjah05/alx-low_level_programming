cf5d57d32247be555d843d9efc71e873d98311f2
benjah05 <senzelebenjamine@gmail.com>
2024-08-08 13:04:56 -0700

    Fix 10, 11 & 100 error handling

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,8 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (!n || index >= sizeof(n) * 8)
 		return (-1);
 	mask = 1 << index;
-	if (!mask)
-		return (-1);
 	*n = *n | mask;
 	return (1);
 }
