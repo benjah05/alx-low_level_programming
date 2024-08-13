3ead9bb234261b0753440a5f9bd1470f69c0cc9b
benjah05 <senzelebenjamine@gmail.com>
2024-08-12 19:48:54 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,4 +5,39 @@
 #include <fcntl.h>
 #include <elf.h>
 /**
+ * main - display information contained in the ELF header
+ * at the start of an ELF file
+ * @argc: number of CL arguments
+ * @argv: argument vector(an array of CL arguments)
+ * Return: 0
  */
+int main(int argc, char *argv[])
+{
+	int fd;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Cant open file %s\n", argv[1]);
+		exit(98);
+	}
+	if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read ELF header\n");
+		close(fd);
+		exit(98);
+	}
+	if (memcpy(elf_header.e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: %s is not an ELF file\n", argv[1]);
+		clode(fd);
+		exit(98);
+	}
+	close(fd);
+	return (0);
+}
