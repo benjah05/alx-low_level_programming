b1ca3510b19f924857c331b28c4e504bcf90bb49
benjah05 <senzelebenjamine@gmail.com>
2024-09-20 04:09:22 -0700

    Insert a node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,8 +26,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			newNode->next = curr;
 			if (curr->prev)
 				curr->prev->next = newNode;
-			else
-				curr->prev = newNode;
 			return (newNode);
 		}
 		index++;
