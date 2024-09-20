2d47859f149a2b1952d61d6495d6ecabe17c4f3e
benjah05 <senzelebenjamine@gmail.com>
2024-09-20 04:56:26 -0700

    Inswrt a node anywhere

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,8 +11,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr, *newNode;
 	unsigned int index = 0;
 
-	if (*h == NULL)
-		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	newNode = malloc(sizeof(dlistint_t));
