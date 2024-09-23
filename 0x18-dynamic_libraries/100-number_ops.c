3aed281d1fb7ab0f5fb0e4db32e6c6307113c04a
benjah05 <senzelebenjamine@gmail.com>
2024-09-23 15:01:27 -0700

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-number_ops.c
@@ -0,0 +1,51 @@
+#include "operators.h"
+/**
+ * add - add 2 numbers
+ * @x: first number
+ * @y: second number
+ * Return: the sum of x and y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+/**
+ * sub - subtract 2 numbers
+ * @x: first number
+ * @y: second number
+ * Return: the difference between x and y
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+ * mul - multiply 2 numbers
+ * @x: first number
+ * @y: second number
+ * Return: the product of x and y
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+ * div - divide 2 numbers
+ * @x: first number
+ * @y: second number
+ * Return: the quotient of x and y
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+/**
+ * mod - modulus/ remainder of 2 numbers
+ * @x: first number
+ * @y: second number
+ * Return: the remainder of x / y
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
---
+++ 0x18-dynamic_libraries/operators.h
@@ -0,0 +1,11 @@
+#ifndef _OPERATORS_H
+#define _OPERATORS_H
+
+#include <stdio.h>
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
+int mod(int x, int y);
+
+#endif
