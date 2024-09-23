2443bfae52d0741b6d4abe697cda5a393fee4170
benjah05 <senzelebenjamine@gmail.com>
2024-09-23 13:28:09 -0700

    What have we?

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -fPIC -c *.c | gcc -shared -o liball.so *.o | export LD_LIBRARY_PATH= .:$LD_LIBRARY_PATH
+gcc -fPIC -c *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH= .:$LD_LIBRARY_PATH
