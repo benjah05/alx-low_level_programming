fe471c29113a1eb6dab258cfd3a12419f12cda53
benjah05 <senzelebenjamine@gmail.com>
2024-09-27 15:29:43 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/gigalib.c
+++ 0x18-dynamic_libraries/gigalib.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int winning_numbers[] = {9, 8, 10, 24, 75};
-int bonus_number = 9;
-int call_count = 0;
-
-int rand()
+/**
+ * rand - override rand() function
+ * Return: winning numbers and bonus
+ */
+int rand(void)
 {
+	int winning_numbers[] = {9, 8, 10, 24, 75};
+	int bonus_number = 9;
+	int call_number = 0;
+
 	if (call_count < 5)
-		return winning_numbers[call_count++];
+		return (winning_numbers[call_count++]);
 	else
-		return bonus_number;
+		return (bonus_number);
 }
+/**
+ * srand - override srand() function
+ * @seed: seed for random number generation
+ * Return: void
+ */
 void srand(unsigned int seed)
 {
-    /* Do nothing */
+	(void)seed;
+	/* Do nothing */
 }
