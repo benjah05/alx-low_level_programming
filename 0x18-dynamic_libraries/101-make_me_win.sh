bd19346a58dc723200ec5a84e86128597f605965
benjah05 <senzelebenjamine@gmail.com>
2024-09-27 15:33:03 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/benjah05/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
-export LD_PRELOAD=/tmp/gigalib.so
+export LD_PRELOAD=$PWD/gigalib.so
