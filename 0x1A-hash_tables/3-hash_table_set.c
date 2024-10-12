41e1466616b0d4d817e6b110731091fc8a6ac745
benjah05 <senzelebenjamine@gmail.com>
2024-10-12 02:57:09 -0700

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,11 +8,25 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newElt;
+	hash_node_t *newElt, *tmp;
 	unsigned long int newIndex;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
+	newIndex = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[newIndex];
+	if (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	newElt = malloc(sizeof(hash_node_t));
 	if (newElt == NULL)
 		return (0);
@@ -29,10 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(newElt);
 		return (0);
 	}
-	newElt->next = NULL;
-	newIndex = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[newIndex] != NULL)
-		newElt->next = ht->array[newIndex];
+	newElt->next = ht->array[newIndex];
 	ht->array[newIndex] = newElt;
 	return (1);
 }
